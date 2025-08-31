b223dc387f52e47bbf53325ddc117750dcfcbbf6
5plus <mshnptrvc@gmail.com>
2025-08-31 20:12:26 +0200

    Update and rename index.tsx to index.js

--- pages/index.js
+++ pages/index.js
@@ -22,8 +22,8 @@ const Popup = dynamic(
 
 // Supabase client
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
 export default function Home() {
